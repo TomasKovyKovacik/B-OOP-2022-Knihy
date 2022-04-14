63d54147291e9415a2dccedc007f01edfce323ab
TomasKovyKovacik <tomas_kovacik@stuba.sk>
2022-04-14 14:49:14 +0200

    add some more tests

--- src/test/java/sk/stuba/fei/uim/oop/assignment3/Assignment3ApplicationTests.java
+++ src/test/java/sk/stuba/fei/uim/oop/assignment3/Assignment3ApplicationTests.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -327,8 +328,292 @@ class Assignment3ApplicationTests {
 
     @Test
     void testAddShoppingCart() throws Exception {
-//        addCart();
+        addList();
     }
+
+    @Test
+    void testGetListById() throws Exception {
+        TestListResponse list = addList();
+        MvcResult mvcResult = mockMvc.perform(get("/list/" + list.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+        TestListResponse listResponse = stringToObject(mvcResult, TestListResponse.class);
+        assert Objects.equals(listResponse.getId(), list.getId());
+    }
+
+    @Test
+    void testGetMissingListById() throws Exception {
+        TestListResponse list = addList();
+        mockMvc.perform(get("/list/" + list.getId() + 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testGetAllLists() throws Exception {
+        MvcResult result = mockMvc.perform(get("/list")
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+        addList();
+        addList();
+        mockMvc.perform(get("/list")
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andDo(mvcResult -> {
+            var list = stringToObject(mvcResult, ArrayList.class);
+            assert list.size() == 2 + stringToObject(result, ArrayList.class).size();
+        });
+    }
+
+    @Test
+    void testDeleteListById() throws Exception {
+        TestListResponse list = addList();
+        mockMvc.perform(delete("/list/" + list.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+        mockMvc.perform(get("/list/" + list.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testDeleteMissingListById() throws Exception {
+        TestListResponse list = addList();
+        mockMvc.perform(delete("/list/" + list.getId() + 1)
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void addBookToList() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        assert list.getLendingList().isEmpty();
+        TestListResponse updatedList = addBookToList(book, list);
+        assert updatedList.getLendingList().size() == 1;
+        assert updatedList.getLendingList().get(0).getId() == book.getId();
+    }
+
+    @Test
+    void addBookToListMissingBook() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        addBookToList(book.getId() + 1, list.getId(), status().isNotFound());
+    }
+
+    @Test
+    void addBookToListMissingList() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        addBookToList(book.getId() , list.getId() + 1, status().isNotFound());
+    }
+
+    @Test
+    void addBookToListExistingBook() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        addBookToList(book, list);
+        addBookToList(book.getId() , list.getId() , status().isBadRequest());
+
+        MvcResult mvcResult = mockMvc.perform(get("/list/" + list.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+        TestListResponse listResponse = stringToObject(mvcResult, TestListResponse.class);
+
+        assert listResponse.getLendingList().size() == 1;
+        assert listResponse.getLendingList().get(0).getId() == book.getId();
+        assert listResponse.getLendingList().get(0).getLendCount() == book.getLendCount();
+    }
+
+    @Test
+    void removeBookFromList() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        assert list.getLendingList().isEmpty();
+        TestListResponse updatedList = addBookToList(book, list);
+        assert updatedList.getLendingList().size() == 1;
+        assert updatedList.getLendingList().get(0).getId() == book.getId();
+
+        TestBookIdRequest bookID = new TestBookIdRequest();
+        book.setId(book.getId());
+        mockMvc.perform(delete("/list/" + list.getId() + "/remove")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectToString(bookID)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        MvcResult mvcResult = mockMvc.perform(get("/list/" + list.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+        TestListResponse listResponse = stringToObject(mvcResult, TestListResponse.class);
+
+        assert listResponse.getLendingList().size() == 0;
+    }
+
+    @Test
+    void testLendList() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        TestBookResponse book2 = addBook();
+        addBookToList(book, list);
+        addBookToList(book2, list);
+        mockMvc.perform(get("/list/" + list.getId() + "/lend")
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        MvcResult mvcResult = mockMvc.perform(get("/list/" + list.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+        TestListResponse listResponse = stringToObject(mvcResult, TestListResponse.class);
+
+        assert listResponse.getLendingList().size() == 2;
+        assert listResponse.getLendingList().get(0).getId() == book.getId();
+        assert listResponse.getLendingList().get(1).getId() == book2.getId();
+        assert listResponse.getLendingList().get(0).getLendCount() == book.getLendCount() + 1;
+        assert listResponse.getLendingList().get(1).getLendCount() == book2.getLendCount() + 1;
+        assert listResponse.isLended();
+    }
+
+    @Test
+    void testLendMissingList() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        addBookToList(book, list);
+        mockMvc.perform(get("/list/" + (list.getId() + 1) + "/lend")
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testLendListTwice() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        addBookToList(book, list);
+        mockMvc.perform(get("/list/" + list.getId() + "/lend")
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+        mockMvc.perform(get("/list/" + list.getId() + "/lend")
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void addBookToLendedList() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        addBookToList(book, list);
+        mockMvc.perform(get("/list/" + list.getId() + "/lend")
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+        TestBookResponse book2 = addBook();
+        addBookToList(book2.getId() , list.getId() , status().isBadRequest());
+
+        mockMvc.perform(get("/book/" + book.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andDo(mvcResult -> {
+                    TestBookResponse bookToControl = stringToObject(mvcResult, TestBookResponse.class);
+                    assert Objects.equals(bookToControl.getId(), book.getId());
+                    assert bookToControl.getLendCount() == book.getLendCount() + 1;
+                });
+        mockMvc.perform(get("/book/" + book2.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andDo(mvcResult -> {
+                    TestBookResponse bookToControl = stringToObject(mvcResult, TestBookResponse.class);
+                    assert Objects.equals(bookToControl.getId(), book2.getId());
+                    assert bookToControl.getLendCount() == book2.getLendCount();
+                });
+        MvcResult mvcResult = mockMvc.perform(get("/list/" + list.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+        TestListResponse listResponse = stringToObject(mvcResult, TestListResponse.class);
+
+        assert listResponse.getLendingList().size() == 1;
+    }
+
+    @Test
+    void testRemoveUnlendedList() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        TestBookResponse book2 = addBook();
+        addBookToList(book, list);
+        TestListResponse listResponse = addBookToList(book2, list);
+
+        assert listResponse.getLendingList().size() == 2;
+        assert listResponse.getLendingList().get(0).getId() == book.getId();
+        assert listResponse.getLendingList().get(1).getId() == book2.getId();
+
+        mockMvc.perform(delete("/list/" + list.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        mockMvc.perform(get("/list/" + list.getId())
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNotFound());
+    }
+
+    @Test
+    void testRemoveLendedList() throws Exception {
+        TestListResponse list = addList();
+        TestBookResponse book = addBook();
+        TestBookResponse book2 = addBook();
+        addBookToList(book, list);
+        addBookToList(book2, list);
+
+        mockMvc.perform(get("/list/" + list.getId() + "/lend")
+                .accept(MediaType.TEXT_PLAIN)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk());
+
+        MvcResult mvcResult = mockMvc.perform(get("/list/" + list.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+        TestListResponse listResponse = stringToObject(mvcResult, TestListResponse.class);
+
+        assert listResponse.getLendingList().size() == 2;
+        assert listResponse.getLendingList().get(0).getId() == book.getId();
+        assert listResponse.getLendingList().get(1).getId() == book2.getId();
+        assert listResponse.getLendingList().get(0).getLendCount() == book.getLendCount() + 1;
+        assert listResponse.getLendingList().get(1).getLendCount() == book2.getLendCount() + 1;
+
+        mockMvc.perform(delete("/list/" + list.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn();
+
+        mockMvc.perform(get("/book/" + book.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andDo(mvcResult2 -> {
+                    TestBookResponse bookToControl = stringToObject(mvcResult2, TestBookResponse.class);
+                    assert Objects.equals(bookToControl.getId(), book.getId());
+                    assert bookToControl.getLendCount() == book.getLendCount();
+                });
+        mockMvc.perform(get("/book/" + book2.getId())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk()).andDo(mvcResult2 -> {
+                    TestBookResponse bookToControl = stringToObject(mvcResult2, TestBookResponse.class);
+                    assert Objects.equals(bookToControl.getId(), book2.getId());
+                    assert bookToControl.getLendCount() == book2.getLendCount();
+                });
+    }
+
+    ///////////////////////////////////////////////////////////
 
     TestAuthorResponse addAuthor() throws Exception {
         return addAuthor("name", "surname", status().is2xxSuccessful());
@@ -383,6 +668,41 @@ class Assignment3ApplicationTests {
         return stringToObject(mvcResult, TestBookResponse.class);
     }
 
+    TestListResponse addList() throws Exception {
+        return addList(status().is2xxSuccessful());
+    }
+
+    TestListResponse addList(ResultMatcher statusMatcher) throws Exception {
+        MvcResult mvcResult = mockMvc.perform(post("/list")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(statusMatcher)
+                .andReturn();
+        return stringToObject(mvcResult, TestListResponse.class);
+    }
+
+    TestListResponse addBookToList(TestBookResponse book, TestListResponse list) throws Exception {
+        return addBookToList(book, list, status().isOk());
+    }
+
+    TestListResponse addBookToList(TestBookResponse book, TestListResponse list,  ResultMatcher statusMatcher) throws Exception {
+        return addBookToList(book.getId(), list.getId(), statusMatcher);
+    }
+
+    TestListResponse addBookToList(long bookId, long listId, ResultMatcher statusMatcher) throws Exception {
+        TestBookIdRequest book = new TestBookIdRequest();
+        book.setId(bookId);
+        MvcResult mvcResult = mockMvc.perform(post("/list/" + listId + "/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .content(objectToString(book)))
+                .andExpect(statusMatcher)
+                .andReturn();
+        if (mvcResult.getResponse().getStatus() == HttpStatus.OK.value()) {
+            return stringToObject(mvcResult, TestListResponse.class);
+        }
+        return null;
+    }
+
     static String objectToString(Object object) {
         try {
             return new ObjectMapper().writeValueAsString(object);
@@ -435,4 +755,19 @@ class Assignment3ApplicationTests {
         protected long id;
         protected List<TestBookResponse> books;
     }
+
+    @Getter
+    @Setter
+    private static class TestListResponse {
+        private long id;
+        private List<TestBookResponse> lendingList;
+        private boolean lended;
+    }
+
+    @Getter
+    @Setter
+    public static class TestBookIdRequest {
+        private long id;
+    }
+
 }
